914b5a54cd56895139d40bfae720ab14250a57c8
fda2026 <jamil.ceo.abiodun@gmail.com>
2026-03-25 13:56:00 +0100

    Modify helm command to unisntall

--- helm/prometheus.sh
+++ helm/prometheus.sh
@@ -5,7 +5,8 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 #helm install prometheus prometheus-community/prometheus \
-helm uninstall monitoring prometheus-community/kube-prometheus-stack \
+helm uninstall monitoring -n monitoring
+
 helm install monitoring prometheus-community/kube-prometheus-stack \
 -f monitoring/prometheus-values.yaml \
 -f monitoring/values.yaml \
